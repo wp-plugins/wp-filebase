d9ff8bb9b9e5c32280e046cb7c0a4aa34537bd4a
fabifott <fabifott@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-21 13:28:31 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-filebase/trunk@220343 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-filebase.php
+++ wp-filebase.php
@@ -4,11 +4,11 @@ Plugin Name: WP-Filebase
 Plugin URI: http://fabi.me/wordpress-plugins/wp-filebase-file-download-manager/
 Description: A powerful download manager supporting file categories, thumbnails, traffic/bit rate limits and more.
 Author: Fabian Schlieper
-Version: 0.1.3.2
+Version: 0.1.3.3
 Author URI: http://fabi.me/
 */
 
-define('WPFB_VERSION', '0.1.3.2');
+define('WPFB_VERSION', '0.1.3.3');
 
 // db settings
 if(isset($wpdb))
