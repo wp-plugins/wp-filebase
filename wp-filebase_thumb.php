1f713a4ed6c6d3b135f97e3f77de61c9652e3146
fabifott <fabifott@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-28 22:28:59 +0000

    WP-Filebase 0.2.8.3 * Fixed context menu for file links pointing to the associated page/post * Images are considered as thumbnails for files with the same name when syncing * Attachment listing now works outside the loop on a single post/page
    git-svn-id: https://plugins.svn.wordpress.org/wp-filebase/tags/0.2.8.3@378602 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-filebase_thumb.php
@@ -0,0 +1,36 @@
+<?php
+require(dirname(__FILE__).'/../../../wp-config.php');
+wpfb_loadclass('File','Category','Download');
+
+$item = null;
+
+if(isset($_GET['fid'])) {
+	$fid = intval($_GET['fid']);
+	
+	if($fid == 0) {
+		$img_path = ABSPATH . WPINC . '/images/';
+		if(file_exists($img = $img_path.'crystal/default.png')
+			|| file_exists($img = $img_path.'default.png')
+			|| file_exists($img = $img_path.'blank.gif')
+		) WPFB_Download::SendFile($img);
+		exit;
+	}
+	
+	$item = WPFB_File::GetFile($fid);
+} elseif(isset($_GET['cid']))
+	$item = WPFB_Category::GetCat(intval($_GET['cid']));
+	
+if($item == null || !$item->CurUserCanAccess(true))
+	exit;
+	
+// if no thumbnail, redirect
+if(empty($item->file_thumbnail) && empty($item->cat_icon))
+{
+	header('Location: ' . $item->GetIconUrl());
+	exit;
+}
+
+// send thumbnail
+WPFB_Download::SendFile($item->GetThumbPath());
+
+?>
