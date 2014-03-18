50b10f776273831418294e8abadb6fb7cc72659f
fabifott <fabifott@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-18 21:18:18 +0000

    = 0.3.0.05 = * Reverted 'direct access block' since this is not working for multi-site in some cases
    git-svn-id: https://plugins.svn.wordpress.org/wp-filebase/tags/0.3.0.05@877478 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpfb-load.php
@@ -0,0 +1,74 @@
+<?php
+
+define('SUPPRESS_LOADING_OUTPUT', empty($_REQUEST['noob']));
+define('NGG_DISABLE_RESOURCE_MANAGER', true); // NexGen Gallery: ne resource manager
+
+error_reporting(0);
+
+function wpfb_on_shutdown()
+{
+	 $error = error_get_last( );
+	 if( $error && $error['type'] != E_STRICT && $error['type'] != E_NOTICE && $error['type'] != E_WARNING  ) {
+		 $func = function_exists('wpfb_ajax_die') ? 'wpfb_ajax_die' : 'wp_die';
+		 $func(json_encode($error));
+	 } else { return true; }
+}
+register_shutdown_function('wpfb_on_shutdown');
+
+if((!empty($_REQUEST['fastload']) || (defined('FASTLOAD'))) && !defined('WP_INSTALLING'))
+	define('WP_INSTALLING', true); // make wp load faster
+
+if(SUPPRESS_LOADING_OUTPUT)
+	@ob_start();
+
+if ( defined('ABSPATH') )
+	require_once(ABSPATH . 'wp-load.php');
+else
+	require_once(dirname(__FILE__).'/../../../wp-load.php');
+
+error_reporting(0);
+
+// check if WP-Filebase is active
+/*
+require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+$wpfb_rpath = basename(untrailingslashit(dirname(__FILE__))).'/wp-filebase.php';
+if(!is_plugin_active($wpfb_rpath))
+	wp_die("WP-Filebase ($wpfb_rpath) not active.<!-- FATAL ERROR: WP-Filebase DISABLED -->");
+ * 
+ */
+
+
+if(defined('WP_ADMIN') && WP_ADMIN) {
+	require_once(ABSPATH.'wp-admin/admin.php');
+} else {
+	if(!function_exists('get_current_screen')) {
+		function get_current_screen() { return null; }
+	}
+	if(!function_exists('add_meta_box')) {
+		function add_meta_box() { return null; }
+	}
+}
+
+
+if(SUPPRESS_LOADING_OUTPUT) {
+	while(@ob_end_clean()){} // destroy all ob buffers
+}
+
+
+if(defined('WP_INSTALLING') && WP_INSTALLING) {
+	require_once(dirname(__FILE__).'/wp-filebase.php'); // load wp-filebase only, no other plugins
+	wpfb_loadclass('Core');
+}
+
+function wpfb_ajax_die($msg,$title='',$args='') {
+	@ob_end_clean();
+	echo '<div class="error-div">
+	<strong>' . $msg . '</strong></div>';
+	exit;	
+}
+
+
+if(defined('DOING_AJAX') && DOING_AJAX) {
+	error_reporting(0);
+	add_filter('wp_die_ajax_handler', create_function('$v','return "wpfb_ajax_die";'));
+}
